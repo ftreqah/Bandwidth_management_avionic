3870a493743e9aa54b4473e3f659e753d0c8a45c
Ben Fouad <benamranefouade@gmail.com>
2020-07-02 01:10:56 +0100

    mininet topo description added.

--- bw_manager_topo.py
+++ bw_manager_topo.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
 
 """
-This example shows a Mininet topology simulating avionic cookpit network.
+This example shows a Mininet topology simulating 
+avionic cookpit network.
 """
 
 from mininet.net import Mininet
